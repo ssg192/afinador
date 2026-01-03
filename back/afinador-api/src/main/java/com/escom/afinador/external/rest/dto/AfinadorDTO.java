4d2f7073057cd1f537cf3de3abf1ab357a0458eb
Gerardo Hernandez <gerahdez1807@gmail.com>
2026-01-03 15:49:50 -0600

    feat:frontend

--- back/afinador-api/src/main/java/com/escom/afinador/external/rest/dto/AfinadorDTO.java
+++ back/afinador-api/src/main/java/com/escom/afinador/external/rest/dto/AfinadorDTO.java
@@ -3,6 +3,7 @@ package com.escom.afinador.external.rest.dto;
 import com.escom.afinador.core.entity.Afinador;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,6 +12,7 @@ import lombok.Setter;
 public class AfinadorDTO {
     @JsonProperty
     @NotNull(message = "CE_RNS002")
+    @Positive(message = "CE_RNS002")
     private Float hz;
     @JsonProperty
     private String nota;
