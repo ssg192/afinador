771e71f09409c3dce310671314e42ffed3e9875d
Salvador Sandoval Garibay <emeth.salvador@ceneval.edu.mx>
2026-01-07 15:47:59 -0600

    feat: desglose

--- back/afinador-api/src/main/java/com/escom/afinador/external/rest/config/CorsFilter.java
+++ back/afinador-api/src/main/java/com/escom/afinador/external/rest/config/CorsFilter.java
@@ -14,7 +14,7 @@ public class CorsFilter implements ContainerResponseFilter {
             ContainerResponseContext responseContext) {
 
         responseContext.getHeaders().putSingle(
-                "Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+                "Access-Control-Allow-Origin", "https://afinador-pds.netlify.app/");
         responseContext.getHeaders().putSingle(
                 "Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
         responseContext.getHeaders().putSingle(
